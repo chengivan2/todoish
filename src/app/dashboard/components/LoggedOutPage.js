203f4f37b318f28986040de8d659dd5d0e53b39b
chengivan <chengoleivan@gmail.com>
2025-01-05 10:37:14 +0300

    Added logged out page

---
+++ src/app/dashboard/components/LoggedOutPage.js
@@ -0,0 +1,15 @@
+'use client';
+
+import Authentication from "../Authentication";
+
+export default function LoggedOutPage() {
+  return (
+    <div className="logged-out-page">
+      <h1>You are logged out</h1>
+      <p>Please log in to access your dashboard.</p>
+      <div className="button-group">
+        <Authentication />
+      </div>
+    </div>
+  );
+} 
