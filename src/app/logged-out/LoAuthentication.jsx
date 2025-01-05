4ef1936987e1e6dceacbbe7a4b520e4e30870bc5
chengivan <chengoleivan@gmail.com>
2025-01-05 11:01:44 +0300

    Styled logged out page

---
+++ src/app/logged-out/LoAuthentication.jsx
@@ -0,0 +1,33 @@
+"use client";
+
+import {
+  LoginLink,
+  RegisterLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import "../dashboard/authentication.css";
+
+export default function Authentication() {
+  return (
+    <div className="container">
+      <div className="authentication-container">
+        <h1>Youve been logged out successfully</h1>
+        
+        <p className="dashboard-welcome-message">
+          Sign in again to continue managing your tasks
+        </p>
+
+        <div className="dashboard-sign-in">
+          <LoginLink>Sign in</LoginLink>
+        </div>
+
+        <div className="separator">
+          <span>OR</span>
+        </div>
+
+        <div className="dashboard-sign-up">
+          <RegisterLink>Sign up</RegisterLink>
+        </div>
+      </div>
+    </div>
+  );
+}
