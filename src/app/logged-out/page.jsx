4dbf791a398789cc0b2de21d7e461fdedd39605e
chengivan <chengoleivan@gmail.com>
2025-01-05 11:06:24 +0300

    Styled logged out component

--- src/app/logged-out/page.jsx
+++ src/app/logged-out/page.jsx
@@ -1,15 +1,9 @@
-'use client';
-
 import LoAuthentication from "./LoAuthentication";
 
 export default function LoggedOutPage() {
   return (
     <div className="logged-out-page">
-      <h1>You are logged out</h1>
-      <p>Please log in to access your dashboard.</p>
-      <div className="button-group">
         <LoAuthentication />
-      </div>
     </div>
   );
 } 
