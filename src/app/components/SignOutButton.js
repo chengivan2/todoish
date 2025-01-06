c7a959fd2de4919ac9a7153ead1c604db2d44204
chengivan <chengoleivan@gmail.com>
2025-01-06 21:05:28 +0300

    Changed logged out link

--- src/app/components/SignOutButton.js
+++ src/app/components/SignOutButton.js
@@ -4,5 +4,5 @@ import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
 import './SignOutButton.css';
 
 export default function SignOutButton() {
-  return <LogoutLink className="sign-out-button">Sign Out</LogoutLink>;
+  return <LogoutLink postLoginRedirectUrl="/logged-out" className="sign-out-button">Sign Out</LogoutLink>;
 }
