7974ca6cf61530f64997097dfeb9caa2b402babd
chengivan <chengoleivan@gmail.com>
2025-01-06 21:26:04 +0300

    Trying sign out callback url

--- src/app/components/SignOutButton.js
+++ src/app/components/SignOutButton.js
@@ -4,5 +4,5 @@ import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
 import './SignOutButton.css';
 
 export default function SignOutButton() {
-  return <LogoutLink postLoginRedirectUrl="/logged-out" className="sign-out-button">Sign Out</LogoutLink>;
+  return <LogoutLink postLogoutRedirectUrl="/logged-out" className="sign-out-button">Sign Out</LogoutLink>;
 }
