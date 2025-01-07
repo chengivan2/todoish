0f1c1bccd3b0d30aeda5824c69ec368a5e4b568c
chengivan <chengoleivan@gmail.com>
2025-01-07 21:03:39 +0300

    Added tasks cards

---
+++ src/app/dashboard/components/TasksCards.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function TasksCards() {
+  return (
+    <>
+      <div className="deleted-tasks-card">
+        <h3>Deleted Tasks</h3>
+        <p>View your deleted tasks here</p>
+      </div>
+
+      <div className="completed-tasks-card">
+        <h3>Completed Tasks</h3>
+        <p>View your completed tasks here</p>
+      </div>
+      
+      <div className="pending-tasks-card">
+        <h3>Pending Tasks</h3>
+        <p>View your pending tasks here</p>
+      </div>
+    </>
+  );
+}
