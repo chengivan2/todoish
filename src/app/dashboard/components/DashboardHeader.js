337e17d7732852f94bff05ce0e05088e632ce9f3
chengivan <chengoleivan@gmail.com>
2025-01-18 04:38:21 +0300

    Replaced text with image on logo container

--- src/app/dashboard/components/DashboardHeader.js
+++ src/app/dashboard/components/DashboardHeader.js
@@ -3,11 +3,18 @@
 import "./DashboardHeader.css";
 import SignOutButton from "@/app/components/SignOutButton";
 import DashboardMenu from "./DashboardMenu";
+import Image from "next/image";
 
 export default function DashboardHeader() {
   return (
     <header className="dashboard-header">
-      <div className="logo">Todoish Dashboard</div>
+      <div className="logo">
+        <Image 
+        src="./todoishMainLogo.png"
+        width={20}
+        height={20}
+        alt="Todoish main logo with a transparent background"/>
+      </div>
       <div className="dashboard-menu-and-sign-out">
       <DashboardMenu className="dashboard-menu" />
       <SignOutButton className="dashboard-sign-out" />
