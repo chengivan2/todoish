66416e7250e0d59b0f9f943d90bde260b00b3960
chengivan <chengoleivan@gmail.com>
2025-01-18 22:47:12 +0300

    Replaced AlertDialog.Title with todoish main logo

--- src/app/dashboard/components/DashboardMenu.jsx
+++ src/app/dashboard/components/DashboardMenu.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import * as AlertDialog from "@radix-ui/react-alert-dialog";
 import "./dashboardmenu.css";
 import { Cross1Icon, HamburgerMenuIcon } from "@radix-ui/react-icons";
+import todoishMainLogo from "./todoishMainLogo.png";
 
 const DashboardMenu = () => (
   <AlertDialog.Root>
@@ -10,12 +11,16 @@ const DashboardMenu = () => (
     </AlertDialog.Trigger>
     <AlertDialog.Portal>
       <AlertDialog.Overlay className="AlertDialogOverlay" />
-      <AlertDialog.Content className="AlertDialogContent">
-
-        <div className="dashboard-menu-title-and-cancel-container">
-          <AlertDialog.Title className="AlertDialogTitle">
-            Todoish
-          </AlertDialog.Title>
+      <AlertDialog.Content aria-label="Todoish Menu" className="AlertDialogContent">
+        <div className="dashboard-menu-logo-and-cancel-container">
+          <div className="dashboard-menu-logo">
+            <Image
+              src={todoishMainLogo}
+              width={60}
+              height={60}
+              alt="Todoish main logo with a transparent background"
+            />
+          </div>
 
           <div className="dashboard-menu-close-icon-container">
             <AlertDialog.Cancel asChild>
@@ -25,12 +30,19 @@ const DashboardMenu = () => (
         </div>
 
         <div className="dashboard-menu-links-container">
-          <AlertDialog.Action><a href="#incomplete-tasks-card">Incomplete Tasks</a></AlertDialog.Action>
-          <AlertDialog.Action><a href="#completed-tasks-card">Completed Tasks</a></AlertDialog.Action>
-          <AlertDialog.Action><a href="#deleted-tasks-card">Deleted Tasks</a></AlertDialog.Action>
-          <AlertDialog.Action><a href="#stats">Stats</a></AlertDialog.Action>
+          <AlertDialog.Action>
+            <a href="#incomplete-tasks-card">Incomplete Tasks</a>
+          </AlertDialog.Action>
+          <AlertDialog.Action>
+            <a href="#completed-tasks-card">Completed Tasks</a>
+          </AlertDialog.Action>
+          <AlertDialog.Action>
+            <a href="#deleted-tasks-card">Deleted Tasks</a>
+          </AlertDialog.Action>
+          <AlertDialog.Action>
+            <a href="#stats">Stats</a>
+          </AlertDialog.Action>
         </div>
-
       </AlertDialog.Content>
     </AlertDialog.Portal>
   </AlertDialog.Root>
