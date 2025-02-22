3cc0d6f0e5713984120a8bd1e2233b7268b6d278
chengivan <chengoleivan@gmail.com>
2025-02-22 08:03:06 +0300

    Styled the hero section

--- src/app/page.js
+++ src/app/page.js
@@ -7,36 +7,38 @@ export default function Home() {
     <div className={styles.page}>
       <main>
         <section id={styles.heroSection}>
-          <header className={styles.header}>
-            <div className={styles.logo}>
-              <Image
-                src="/todoishMainLogo.png"
-                width={50}
-                height={50}
-                alt="Todoish Main Logo"
-              />
-            </div>
-            <nav className={styles.menu}>
-              <Link className={styles.link} href="/">
-                Home
-              </Link>
-              <Link className={styles.link} href="/about">
-                About
-              </Link>
-              <Link className={styles.link} href="/contact">
-                Contact
-              </Link>
-            </nav>
-            <div className={styles.authButtons}>
-              <Link className={styles.link} href="/dashboard">
-                Dashboard
-              </Link>
-              <button className={styles.signOutButton}>Sign Out</button>
-            </div>
-          </header>
           <div className={styles.hero}>
+            <header className={styles.header}>
+              <div className={styles.logo}>
+                <Image
+                  src="/todoishMainLogo.png"
+                  width={50}
+                  height={50}
+                  alt="Todoish Main Logo"
+                />
+              </div>
+              <nav className={styles.menu}>
+                <Link className={styles.link} href="/">
+                  Home
+                </Link>
+                <Link className={styles.link} href="/about">
+                  About
+                </Link>
+                <Link className={styles.link} href="/contact">
+                  Contact
+                </Link>
+              </nav>
+              <div className={styles.authButtons}>
+                <Link className={styles.link} href="/dashboard">
+                  Dashboard
+                </Link>
+                <button className={styles.signOutButton}>Sign Out</button>
+              </div>
+            </header>
+            <div className={styles.heroSectionText}>
             <h1 className={styles.heroTitle}>Welcome to Todoish</h1>
             <p className={styles.heroSubtitle}>Your task management solution</p>
+            </div>
           </div>
         </section>
       </main>
