a284abe0b2baddb372b6aaaeeb39fb3aa6201989
chengivan <chengoleivan@gmail.com>
2025-02-22 07:49:34 +0300

    Added the hero section and its styles

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,44 @@
 import Link from "next/link";
-import styles from "./page.module.css";
+import styles from "./homepage.module.css";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <h1>Todoish</h1>
-        <p>Go to the dashboard</p>
-        <Link className={styles.link} href="/dashboard">Dashboard</Link>
+      <main>
+        <section id={styles.heroSection}>
+          <header className={styles.header}>
+            <div className={styles.logo}>
+              <Image
+                src="/todoishMainLogo.png"
+                width={50}
+                height={50}
+                alt="Todoish Main Logo"
+              />
+            </div>
+            <nav className={styles.menu}>
+              <Link className={styles.link} href="/">
+                Home
+              </Link>
+              <Link className={styles.link} href="/about">
+                About
+              </Link>
+              <Link className={styles.link} href="/contact">
+                Contact
+              </Link>
+            </nav>
+            <div className={styles.authButtons}>
+              <Link className={styles.link} href="/dashboard">
+                Dashboard
+              </Link>
+              <button className={styles.signOutButton}>Sign Out</button>
+            </div>
+          </header>
+          <div className={styles.hero}>
+            <h1 className={styles.heroTitle}>Welcome to Todoish</h1>
+            <p className={styles.heroSubtitle}>Your task management solution</p>
+          </div>
+        </section>
       </main>
     </div>
   );
